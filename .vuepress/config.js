e6564e652668ec58a03c1cf34e43a63205c7b5a2
shailen <shailen@lumkani.com>
2019-09-10 15:03:38 +0200

    Added nav links

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,6 +1,35 @@
 module.exports = {
   title: 'Lumkani News',
   head: [
-    ['link', { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Poppins&display=swap' }]
-  ]
-}
+    ['link', { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Poppins&display=swap' }],
+    ['link', { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/flexboxgrid/6.3.1/flexboxgrid.min.css' }],
+  ],
+  themeConfig: {
+    logo: 'https://avatars3.githubusercontent.com/u/18303687?s=400&u=cdffc0d3253e9247ed24d5a3e70a712cef71d133&v=4',
+    nav: [
+      {
+        text: 'Stories 📚',
+        link: '/stories/'
+      },
+      {
+        text: 'Tutorials 📼',
+        link: '/tutorials/'
+      },
+      {
+        text: 'Updates ✨',
+        link: '/updates/'
+      },
+      {
+        text: 'Lumkani',
+        link: 'https://lumkani.com'
+      },
+    ],
+    sidebar: [
+      '/stories/',
+      '/tutorials/',
+      '/updates/',
+    ],
+    sidebarDepth: 2,
+    activeHeaderLinks: true,
+  },
+};
--- README.md
+++ README.md
@@ -9,3 +9,44 @@ meta:
     content: Financial inclusion, IoT sensors, resilience through technology
 ---
 
+<br>
+<br>
+
+---
+
+<center>
+<div class="row">
+<div class="col-md-4 col-sm-4 col-xs-12">
+<div class="box">
+
+### Stories 📚
+
+Find out about everything from *"How Lumkani started"* to our *"Open-source initiatives"* :heart:
+
+</div>
+</div>
+<div class="col-md-4 col-sm-4 col-xs-12">
+<div class="box">
+
+### Tutorials 📼
+
+Here are an amazing collection of tutorials we wrote from *Dockerizing apps* to *Doing code reviews*
+
+</div>
+</div>
+<div class="col-md-4 col-sm-4 col-xs-12">
+<div class="box">
+
+### Updates ✨
+
+Find out what is happening at Lumkani right now. We will keep you updated
+
+</div>
+</div>
+</div>
+</center>
+
+<br>
+<br>
+<br>
+<br>
