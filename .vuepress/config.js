7103666d67be68526e15c87ae3ffbd0c91b3051a
Shailen Naidoo <shailen@lumkani.com>
2019-12-04 09:48:12 +0200

    Update config.js

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -8,7 +8,7 @@ module.exports = {
     logo: 'https://avatars3.githubusercontent.com/u/18303687?s=400&u=cdffc0d3253e9247ed24d5a3e70a712cef71d133&v=4',
     nav: [
       {
-        text: 'Stories 📚',
+        text: 'Articles 📚',
         link: '/stories/'
       },
       {
